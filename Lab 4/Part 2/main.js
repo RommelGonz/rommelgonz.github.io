d5c48a280b6ecb7cc06a5319da3eb0b18c42b330
RommelGonz <rommelgonz14@gmail.com>
2025-03-31 14:19:05 -0400

    Update main.js

--- Lab 4/Part 2/main.js
+++ Lab 4/Part 2/main.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const images = ['images/pic1.jpg', 'images/pic2.jpg', 'images/pic3.jpg', 'images/pic4.jpg', 'images/pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 const imgAlts =[ 
@@ -19,7 +19,7 @@ const imgAlts =[
 /* Looping through images */
 for (let i = 0; i < images.length; i++) {
 const newImage = document.createElement('img');
-newImage.setAttribute('src', 'images/${images[i]}');
+displayedImage.setAttribute('src', 'images/${images[i]}');
 newImage.setAttribute('alt', imgAlts[i].altText);
 
 newImage.addEventListener('click', () => {
