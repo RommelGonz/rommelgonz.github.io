df230d71223b9e98f412229b7079795cda39b2bd
RommelGonz <rommelgonz14@gmail.com>
2025-03-31 13:11:55 -0400

    declaring the alternative text for all photos
    declaring the photos for all 5 images

--- Lab 4/Part 2/main.js
+++ Lab 4/Part 2/main.js
@@ -5,10 +5,15 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const imgAlts =[ 
+{altText: "Photo 1 - Big eye"}, {altText: "Photo 2 - Wavy photo"}, {altText: "Photo 3 - Flower"}, {altText: "Photo 4 - Egyptian wall"}, {altText: "Photo 5 - Big Moth"}
+]; 
 
 /* Looping through images */
+
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
